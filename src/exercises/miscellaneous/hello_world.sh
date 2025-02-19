21fbdff192592635d0377febb262d5fa97335ea8
phiewter <19486709+phiewter@users.noreply.github.com>
2025-02-19 22:52:13 +0100

    fix: redefined 'say_hallo' to 'hello_world' script

--- src/exercises/miscellaneous/hello_world.sh
+++ src/exercises/miscellaneous/hello_world.sh
@@ -1,5 +1,8 @@
-#!/bin.bash
+#!/bin/bash
 
-function hello_world() {
-	echo "Hello, World!"
-}
+# Created:	    11/02/2025
+# Modified:	    19/02/2025
+
+# Title:	      Hello, World!
+
+# Description:  Define a function that says 'Hello, World!'.
--- src/exercises/miscellaneous/say_hello.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-say_hello() {
-  echo "Hello, $1"
-}
